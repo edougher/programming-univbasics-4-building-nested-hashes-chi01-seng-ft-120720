8baab674a7cea3b8016019d3733088c582b27653
erin dougher <ejdougher@gmail.com>
2020-11-27 03:56:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
 monopoly = {:railroads => {}}
 monopoly[:railroads] = { :pieces => 4 }
 monopoly[:railroads][:pieces]
-#monopoly
+monopoly
 end
 
 def monopoly_with_third_tier
