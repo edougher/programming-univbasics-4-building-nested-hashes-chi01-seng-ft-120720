b747759f0ccac6dad4e5f448a53b277e8f868130
erin dougher <ejdougher@gmail.com>
2020-11-27 04:15:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
   # complexity.
   monopoly = {:railroads => {}}
   monopoly[:railroads] = { :pieces => 4 }
-  monopoly[:railroads][:rent_in_dollars] = {}
+  monopoly[:railroads][:rent_in_dollars] = {:names}
   monopoly
 
 end
