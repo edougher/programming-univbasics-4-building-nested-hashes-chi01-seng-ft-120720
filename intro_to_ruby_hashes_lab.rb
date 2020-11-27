1150615cb3d045c99c9e6628be9b33fc0ec6b470
erin dougher <ejdougher@gmail.com>
2020-11-27 04:52:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,7 +59,7 @@ monopoly[:railroads][:names][:pennsylvania_railroad] = {}
 monopoly[:railroads][:names][:b_and_o_railroad] = {}
 monopoly[:railroads][:names][:shortline_railroad] = {}
 
-monopoly[:railroads][:names][:reading_railroad][:mortgage_value] = to_n.100
+monopoly[:railroads][:names][:reading_railroad][:mortgage_value] = "100"
 monopoly[:railroads][:names][:pennsylvania_railroad][:mortgage_value] = 200
 monopoly[:railroads][:names][:b_and_o_railroad][:mortgage_value] = 400
 monopoly[:railroads][:names][:shortline_railroad][:mortgage_value] = 800
